84a667510e66f60e45ac7994e8dbce568a9fc251
Dmitriy Sukhanov <dmsuhanov@mail.ru>
2022-02-02 22:30:51 +0500

    first commit

---
+++ src/main/java/com/suhan/cargo/CargoApplication.java
@@ -0,0 +1,11 @@
+package com.suhan.cargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CargoApplication.class, args);
+    }
+}
