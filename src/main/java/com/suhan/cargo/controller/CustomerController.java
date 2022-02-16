66349631b2985ecdd954d2215ec515f274fc61fc
suhan <dmsuhanov@mail.ru>
2022-02-16 22:25:58 +0500

    endpoint add customer

--- src/main/java/com/suhan/cargo/controller/CustomerController.java
+++ src/main/java/com/suhan/cargo/controller/CustomerController.java
@@ -1,12 +1,13 @@
 package com.suhan.cargo.controller;
 
 import com.suhan.cargo.controller.model.CustomerDto;
+import com.suhan.cargo.controller.model.CustomerFormDto;
+import com.suhan.cargo.domain.customer.Customer;
 import com.suhan.cargo.domain.customer.CustomerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/customer")
@@ -18,6 +19,13 @@ public class CustomerController {
     @GetMapping
     public Flux<CustomerDto> getUsers() {
         return customerRepository.findAll().map(a -> new CustomerDto(a.id().orElseThrow(), a.name()));
+    }
+
+    @PostMapping
+    public Mono<CustomerDto> create(@RequestBody CustomerFormDto customerForm) {
+        final Customer customer = new Customer(customerForm.getName());
+        return customerRepository.save(customer)
+                .map(a -> new CustomerDto(a.id().orElseThrow(), a.name()));
     }
 
 }
---
+++ src/main/java/com/suhan/cargo/controller/model/CustomerFormDto.java
@@ -0,0 +1,16 @@
+package com.suhan.cargo.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerFormDto {
+
+    private String name;
+
+}
